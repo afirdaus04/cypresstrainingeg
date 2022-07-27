6ea9d3232c33b6fdc44981e8bdc9a2151d8b5ef8
afirdaus04 <afirdaus.pe@gmail.com>
2022-07-27 08:06:37 +0800

    api test

---
+++ cypress/integration/API Test/apitest.spec.js
@@ -0,0 +1,44 @@
+const { it } = require("mocha");
+
+describe('HTTP Example', function () {
+
+    it('GET',function(){
+        cy.request({
+            method: 'GET',
+            url: 'https://httpbin.org/get',
+
+        }).then(function (response) {
+            
+            expect(response.body).have.property('url');
+
+        });
+
+    });
+
+    it('POST',function(){
+        
+        cy.request({
+            method: 'POST',
+            url: 'https://httpbin.org/post',
+            body: {
+                'name': 'Firdaus',
+                'age': 29
+                
+            },
+            headeers: {
+                'content-type': 'application/json'
+            }
+
+        }).then(function (response) {
+            
+            expect(response.body).have.property('json');
+            expect(response.body.json).to.deep.equal({
+                "name": 'Firdaus',
+                "age": 29
+            });
+
+        });
+
+});
+
+});
